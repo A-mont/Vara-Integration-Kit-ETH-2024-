1e3673ca7aaceaca6a61d579a631ad16dd015312
Arikabz <arturo.guza1999@gmail.com>
2024-02-03 14:53:48 -0600

    homepage overhaul

--- Frontend/src/components/layout/header/Header.tsx
+++ Frontend/src/components/layout/header/Header.tsx
@@ -11,22 +11,7 @@ function Header({ isAccountVisible }: Props) {
                 <a href='/home' className="btn btn-ghost text-xl">daisyUI</a>
             </div>
             <div className="flex-none">
-                <ul className="menu menu-horizontal px-1">
-                    <li>
-                        {isAccountVisible && <Account />}
-                    </li>
-                    <li>
-                        <details>
-                            <summary>
-                                Parent
-                            </summary>
-                            <ul className="p-2 bg-base-100 rounded-t-none">
-                                <li><h1>Link 1</h1></li>
-                                <li><h2>Link 2</h2></li>
-                            </ul>
-                        </details>
-                    </li>
-                </ul>
+                {isAccountVisible && <Account />}
             </div>
         </div>
     );
--- Frontend/src/pages/home/Home.tsx
+++ Frontend/src/pages/home/Home.tsx
@@ -1,33 +1,40 @@
 
 
 function Home() {
- 
+    return (
 
-  return (
-<div className="carousel rounded-box">
-  <div className="carousel-item">
-    <img src="https://daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg" alt="Burger" />
-  </div> 
-  <div className="carousel-item">
-    <img src="https://daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.jpg" alt="Burger" />
-  </div> 
-  <div className="carousel-item">
-    <img src="https://daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.jpg" alt="Burger" />
-  </div> 
-  <div className="carousel-item">
-    <img src="https://daisyui.com/images/stock/photo-1494253109108-2e30c049369b.jpg" alt="Burger" />
-  </div> 
-  <div className="carousel-item">
-    <img src="https://daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.jpg" alt="Burger" />
-  </div> 
-  <div className="carousel-item">
-    <img src="https://daisyui.com/images/stock/photo-1559181567-c3190ca9959b.jpg" alt="Burger" />
-  </div> 
-  <div className="carousel-item">
-    <img src="https://daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg" alt="Burger" />
-  </div>
-</div>
-  )
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content text-center">
+                <div className="max-w-max">
+                    <div className="carousel rounded-box">
+                        <div className="carousel-item">
+                            <img src="https://daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg" alt="Burger" />
+                        </div> 
+                        <div className="carousel-item">
+                            <img src="https://daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.jpg" alt="Burger" />
+                        </div> 
+                        <div className="carousel-item">
+                            <img src="https://daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.jpg" alt="Burger" />
+                        </div> 
+                        <div className="carousel-item">
+                            <img src="https://daisyui.com/images/stock/photo-1494253109108-2e30c049369b.jpg" alt="Burger" />
+                        </div> 
+                        <div className="carousel-item">
+                            <img src="https://daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.jpg" alt="Burger" />
+                        </div> 
+                        <div className="carousel-item">
+                            <img src="https://daisyui.com/images/stock/photo-1559181567-c3190ca9959b.jpg" alt="Burger" />
+                        </div> 
+                        <div className="carousel-item">
+                            <img src="https://daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg" alt="Burger" />
+                        </div>
+                    </div>
+                    <button type='button' className="btn btn-primary">Get Started</button>
+                </div>
+            </div>
+        </div>
+
+    )
 }
 
 export { Home };
