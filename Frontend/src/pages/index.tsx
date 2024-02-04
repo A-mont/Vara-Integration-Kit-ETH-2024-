1d0dc5221fa479d57686fedc02a476c6bf145ad7
D9J9V <macbookair@MACOS-D9J9V.local>
2024-02-03 19:35:35 -0600

    Creating Chat page and updating Index accordingly

--- Frontend/src/pages/index.tsx
+++ Frontend/src/pages/index.tsx
@@ -1,11 +1,19 @@
-import { Route, Routes } from 'react-router-dom';
-import { Home } from './home';
-import { Main } from './main';
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./home";
+import { Chat } from "./chat";
+import { Main } from "./main";
 
-const routes = [{ path: '/', Page: Home },{ path: '/main', Page: Main }];
+const routes = [
+  { path: "/", Page: Home },
+  { path: "/main", Page: Main },
+  { path: "/chat", Page: Chat },
+];
 
 function Routing() {
-  const getRoutes = () => routes.map(({ path, Page }) => <Route key={path} path={path} element={<Page />} />);
+  const getRoutes = () =>
+    routes.map(({ path, Page }) => (
+      <Route key={path} path={path} element={<Page />} />
+    ));
 
   return <Routes>{getRoutes()}</Routes>;
 }
